ef569393a1a13acd726ff78267ce864e03d83194
CheckEZ <hihilolrr@gmail.com>
2026-04-05 17:36:24 +0300

    Fix migrations

--- migrations/versions/dc3978966607_.py
+++ migrations/versions/dc3978966607_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 32f8ec053a43
+Revision ID: dc3978966607
 Revises: 
-Create Date: 2026-03-28 16:38:09.268679
+Create Date: 2026-04-05 17:34:35.195596
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '32f8ec053a43'
+revision: str = 'dc3978966607'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -45,6 +45,7 @@ def upgrade() -> None:
     sa.Column('uuid', sa.String(), nullable=False),
     sa.Column('caller_id', sa.Integer(), nullable=False),
     sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=True),
+    sa.Column('is_private', sa.Boolean(), nullable=False),
     sa.ForeignKeyConstraint(['caller_id'], ['users.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
